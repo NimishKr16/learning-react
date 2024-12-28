56ac1d3a5d117393654e3e875cf1926ea0cdada7
Nimish Kumar <nimishkumar0305@gmail.com>
2024-12-29 02:53:48 +0530

    Basic app starting

---
+++ learning-react-basics/src/App.js
@@ -0,0 +1,13 @@
+import './index.css'
+import './App.css'
+import Counter from './Counter';
+
+const App = () => {
+  return (
+    <div > 
+      <Counter></Counter>
+    </div>
+  );
+}
+
+export default App;
